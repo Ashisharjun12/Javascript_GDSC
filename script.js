23275e43ad3ba469840978f14dfb304173de0f76
Ashish Raj <ashishrahul748@gmail.com>
2024-03-17 19:53:19 +0530

    Javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+// const home = document.querySelector("#home")
+// home.style.color= "red"
+
+// home.innerHTML="<b>hello<b>"
+// home.textContent="<b>hello<b>"
+
+
+// home.addEventListener('click',function(){
+//     alert("event lister")
+// })
+
